08cf0feb9a1e29b94677910c359c54f896eee893
Alex Maranville <alexmaranville@gmail.com>
2025-07-18 12:06:22 -0400

    added a number counter

--- code/test.py
+++ code/test.py
@@ -1,1 +1,8 @@
 print("Hello, world!")
+
+number = 7  # You can change this to test other values
+
+if number > 10:
+    print("Yes, the number is greater than 10.")
+else:
+    print("No, the number is not greater than 10.")
